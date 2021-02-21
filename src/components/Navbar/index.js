8903f2dcea7f1ef06a00c00e4ca0da4dae045c1c
Felipe <felipe98_stz@hotmail..com>
2021-02-21 11:51:33 -0300

    initial project setup

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './navbar.scss';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>About</li>
+                <li>Careers</li>
+                <li>Events</li>
+                <li>Products</li>
+                <li>Support</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
