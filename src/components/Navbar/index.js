ccad202669b7f5a9b319541517ce856199ee8ad8
Felipe <felipe98_stz@hotmail..com>
2021-03-03 11:19:44 -0300

    navbar stylization

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import './navbar.scss';
+import logo from '../../assets/images/logo.svg';
+import hamburger from '../../assets/images/icon-hamburger.svg';
 
 const Navbar = () => {
     return (
-        <nav>
+        <nav className="navContainer">
+            <img src={logo} alt="loopstudios logo" />
+            <button type="button" className="hamburgerButton">
+                <img src={hamburger} alt="hamburger" />
+            </button>
             <ul>
-                <li>About</li>
-                <li>Careers</li>
-                <li>Events</li>
-                <li>Products</li>
-                <li>Support</li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Careers</a>
+                </li>
+                <li>
+                    <a href="#">Events</a>
+                </li>
+                <li>
+                    <a href="#">Products</a>
+                </li>
+                <li>
+                    <a href="#">Support</a>
+                </li>
             </ul>
         </nav>
     )
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './home.scss';
+import Navbar from "../../components/Navbar";
 
 const Home = () => {
     return (
-        <div>
-            
-        </div>
+        <main className="mainHome">
+            <div className="homeContainer .open-menu">
+                <Navbar />
+            </div>
+        </main>
     )
 }
 
